dd61f9c5a16835cc9ab23369cc85c52d985d4752
Lars Riel <larsriel@gmail.com>
2025-02-10 21:20:48 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -5,24 +5,36 @@ set -e
 read -p "Do you want to build the project? (y/n): " choice
 case "$choice" in 
     y|Y ) 
-        echo "Building the project..."
+        echo -e "\nBuilding the project..."
         cargo build --release
         ;;
-    n|N ) echo "Skipping the build.";;
+    n|N ) echo -e "\nSkipping the build.";;
     * ) 
-        echo "Invalid input. Please enter y or n."
+        echo -e "\nInvalid input. Please enter y or n."
         exit 1
         ;;
 esac
 
 INSTALL_DIR="/usr/local/bin"
 
-echo "Copying the binary to $INSTALL_DIR..."
+echo -e "\nCopying the binary to $INSTALL_DIR..."
 sudo cp target/release/clai $INSTALL_DIR
 
-echo "clai has been installed successfully!"
+echo -e "\nclai has been installed successfully!"
 
-read -p "Create aliases? (y/n): " choice
+ALIASES_CREATED=$(grep -c "clai" ~/.bash_aliases)
+if [ $ALIASES_CREATED -gt 0 ]; then
+    exit 0
+fi
+
+echo ""
+echo -e "\033[1mDo you want to add shortcuts to ~/.bash_aliases?\033[0m"
+echo "chat    -> clai chat"
+echo "suggest -> clai suggest"
+echo "explain -> clai explain"
+echo ""
+
+read -p "Accept? (y/n): " choice
 case "$choice" in 
     y|Y ) 
         echo "alias chat='clai chat'" >> ~/.bash_aliases
