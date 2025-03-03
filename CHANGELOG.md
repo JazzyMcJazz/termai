6827134e912365616283e540409b2b88531da58c
Lars Riel <larsriel@gmail.com>
2025-03-03 19:46:07 +0100

    added entry to changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 0.2.2
+
+### Added
+
+- **Shell environment detection on Windows, MacOS and Linux.** This information is used to provide the assistant additional context about the user's environment when suggesting commands or providing other help with in the terminal. Only shell name (e.g. `pwsh`, `bash`) and OS name (e.g. `Windows`, `MacOS`) is provided. No other information about the user's system is automatically provided.
+
 ## 0.2.1
 
 ### Added
--- src/utils/changelog.rs
+++ src/utils/changelog.rs
@@ -1,3 +1,5 @@
+use console::Term;
+use indicatif::TermLike;
 use termimad::MadSkin;
 
 static CHANGELOG: &str = include_str!("../../CHANGELOG.md");
@@ -5,6 +7,8 @@ static SEE_MORE: &str =
     "**See more changelogs at**\nhttps://github.com/JazzyMcJazz/termai/blob/main/CHANGELOG.md";
 
 pub fn print_latest() {
+    let term = Term::stdout();
+
     let sections = CHANGELOG.split("\n## ").collect::<Vec<&str>>();
 
     let latest = if CHANGELOG.contains("## [Unreleased]") {
@@ -16,12 +20,7 @@ pub fn print_latest() {
     if let Some(latest) = latest {
         let skin = MadSkin::default();
         let text = format!("# CHANGELOG\n\n**Version {}**\n{}", latest, SEE_MORE);
-        let longest_line = text
-            .lines()
-            .max_by_key(|line| line.len())
-            .unwrap_or_default()
-            .len();
-        let text = skin.text(&text, Some(longest_line));
+        let text = skin.text(&text, Some(term.width() as usize));
 
         println!("\n{}", text);
     } else {
